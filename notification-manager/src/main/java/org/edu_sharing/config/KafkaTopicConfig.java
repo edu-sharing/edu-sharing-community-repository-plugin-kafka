1d617b019b09cdedb8a1c576e7c15c3d00c73fb3
Sebastian Wollner <wollner@edu-sharing.net>
2023-02-28 14:48:57 +0100

    feat: modified notification topic settings

--- notification-manager/src/main/java/org/edu_sharing/config/KafkaTopicConfig.java
+++ notification-manager/src/main/java/org/edu_sharing/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package org.edu_sharing.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.edu_sharing.AppConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,10 @@ public class KafkaTopicConfig {
      */
     @Bean
     public NewTopic notificationTopic(){
-        return TopicBuilder.name(AppConstants.TOPIC_NAME_NOTIFICATION)
+        return TopicBuilder
+                .name(AppConstants.TOPIC_NAME_NOTIFICATION)
+                .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(1000*60*60*24))
+                .compact()
                 .build();
     }
 }
