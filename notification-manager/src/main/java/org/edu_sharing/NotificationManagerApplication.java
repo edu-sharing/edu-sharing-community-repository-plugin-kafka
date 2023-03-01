be2832f7c785f7268ff6c997ff8acde637376b73
Sebastian Wollner <wollner@edu-sharing.net>
2023-03-01 12:40:26 +0100

    feat: added docker for NotificationManager

--- notification-manager/src/main/java/org/edu_sharing/NotificationManagerApplication.java
+++ notification-manager/src/main/java/org/edu_sharing/NotificationManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationsApplication {
+public class NotificationManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotificationsApplication.class, args);
+        SpringApplication.run(NotificationManagerApplication.class, args);
     }
 
 }
