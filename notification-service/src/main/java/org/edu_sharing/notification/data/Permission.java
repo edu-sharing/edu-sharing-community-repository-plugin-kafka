17489b2f11de868d4dff05573c6d8abb218f2f3e
Sebastian Wollner <wollner@edu-sharing.net>
2023-07-11 12:58:19 +0200

    fix: missing no args constructor

--- notification-service/src/main/java/org/edu_sharing/notification/data/Permission.java
+++ notification-service/src/main/java/org/edu_sharing/notification/data/Permission.java
@@ -2,12 +2,14 @@ package org.edu_sharing.notification.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Jacksonized
 @SuperBuilder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Permission {
     private String permission;
