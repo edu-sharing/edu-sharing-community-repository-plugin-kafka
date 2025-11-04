e782bfab1ba5cf3ce6d82f3af0556355431d4734
Sebastian Wollner <wollner@edu-sharing.net>
2025-11-04 10:46:58 +0100

    fix: use recursive group memberships resolution in KafkaNotificationService

--- Backend/services/module/src/main/java/org/edu_sharing/plugin_kafka/notification/KafkaNotificationService.java
+++ Backend/services/module/src/main/java/org/edu_sharing/plugin_kafka/notification/KafkaNotificationService.java
@@ -86,7 +86,7 @@ public class KafkaNotificationService implements NotificationProxyService {
         List<String> result = new ArrayList<>();
         result.add(authority);
         if (authorityType == AuthorityType.GROUP && resolveGroups) {
-            result.addAll(Arrays.asList(authorityService.getMembershipsOfGroup(authority)));
+            result.addAll(authorityService.getMembershipsOfGroupRecursively(authority));
         }
 
 
